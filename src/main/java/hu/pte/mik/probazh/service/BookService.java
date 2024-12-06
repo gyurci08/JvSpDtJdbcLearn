94b7d2e92a7897599c7e457146cd57e45947bd3d
György Jandzsó <haver1036@gmail.com>
2024-12-06 16:26:37 +0100

    Refactor

--- src/main/java/hu/pte/mik/probazh/service/BookService.java
+++ src/main/java/hu/pte/mik/probazh/service/BookService.java
@@ -5,6 +5,7 @@ import hu.pte.mik.probazh.bean.BookDTO;
 import hu.pte.mik.probazh.database.AuthorRepository;
 import hu.pte.mik.probazh.database.BookRepository;
 import hu.pte.mik.probazh.entity.Author;
+import hu.pte.mik.probazh.entity.Book;
 import hu.pte.mik.probazh.service.mapping.BookMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,15 +26,21 @@ public class BookService {
         return bookRepository.findAll()
                 .stream()
                 .map(book -> {
-                    List<AuthorDTO> authors = book.getAuthors().stream()
-                            .map(ba -> {
-                                Author author = authorRepository.findById(ba.getAuthorId())
-                                        .orElseThrow(() -> new RuntimeException("Author not found!"));
-                                return new AuthorDTO(author.getId(), author.getName());
-                            })
-                            .collect(Collectors.toList());
+                    List<AuthorDTO> authors = mapAuthors(book);
                     return bookMapper.toDTO(book, authors);
                 })
                 .collect(Collectors.toList());
     }
+
+    private List<AuthorDTO> mapAuthors(Book book) {
+        return book.getAuthors().stream()
+                .map(ba -> {
+                    Author author = authorRepository.findById(ba.getAuthorId())
+                            .orElseThrow(() -> new RuntimeException("Author not found!"));
+                    return new AuthorDTO(author.getId(), author.getName());
+                })
+                .collect(Collectors.toList());
+    }
+
+
 }
