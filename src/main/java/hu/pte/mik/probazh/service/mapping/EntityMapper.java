216c71a81f97841b8428c4ef90168d8f54d21a5c
György Jandzsó <haver1036@gmail.com>
2024-12-08 11:12:38 +0100

    List

--- src/main/java/hu/pte/mik/probazh/service/mapping/EntityMapper.java
+++ src/main/java/hu/pte/mik/probazh/service/mapping/EntityMapper.java
@@ -1,4 +1,8 @@
 package hu.pte.mik.probazh.service.mapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * DTO -> entity mapper
  *
@@ -7,4 +11,14 @@ package hu.pte.mik.probazh.service.mapping;
  */
 public interface EntityMapper<E, D> {
     E toEntity(D dto);
+
+    default List<E> toEntity(List<D> dtos) {
+        if (dtos == null) {
+            return List.of();
+        }
+
+        return dtos.stream()
+                .map(this::toEntity)
+                .collect(Collectors.toList());
+    }
 }
